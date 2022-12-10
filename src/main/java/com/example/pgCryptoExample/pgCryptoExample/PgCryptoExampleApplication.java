e5536413d192e9810d128283928335fd85d7c3b4
priyadarshanp <priyadarshanp@sahaj.ai>
2022-12-10 21:06:43 +0530

    new commit

---
+++ src/main/java/com/example/pgCryptoExample/pgCryptoExample/PgCryptoExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.pgCryptoExample.pgCryptoExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PgCryptoExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PgCryptoExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pgCryptoExample/pgCryptoExample/PgCryptoExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pgCryptoExample.pgCryptoExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PgCryptoExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
